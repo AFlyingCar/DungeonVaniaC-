87ecb603256ab62b301c4150032fff266832d708
Tyler Robbins <tyler@familyrobbins.com>
2015-06-21 14:38:16 -0700

    Rewriting entire application.

--- Player.h
+++ Player.h
@@ -1,48 +1,25 @@
-/*
- * Tyler Robbins
- * Player
- * 5/20/15
- * Defines the player class
- */
-
 #ifndef _PLAYER_H_
 #define _PLAYER_H_
 
-#include <stdlib.h>
-#include <time.h>
+#include <string>
 #include <vector>
+#include "DungeonVania.h"
 
 class Player{
- public:
-	Player(char* newName);
+public:
+	Player();
 	~Player();
-	
-	char* getName();
-	std::vector<Item*> getInventory();
-	Item setInventoryItem(int index, Item element);
-	int getMoney();
-	void setMoney(int emoe);
-	void addMoney(int emone);
-	int getHealth();
-	void setHealth(int newHealth);
-	void addHealth(int newHealth);
-	int getArmourAbsorption();
-	int damageEnemy(Enemy* e);
-	char* usePotion();
-	bool buyPotion();
-	bool upgradeArmour();
-	bool upgradeWeapon();
-	void setInventory(std::vector<Item*> inv);
-	bool isDead();
-	void searchRoom(Room* r);
-
- private:
- 	void resetInventory();
-
-	std::vector<Item*> m_inventory;
-	const char* m_NAME;
+	std::string getName();
+protected:
+	void setName(std::string newName);
+private:
+	std::string m_name;
 	int m_money;
 	int m_health;
-};
+	//std::vector<Item> m_inventory;
 
+	void resetInventory();
+public:
+	friend void createNewPlayer(Player*);
+};
 #endif
