12da7b5a3b63d8e5ce8c19016b36ed3add22068d
Tyler Robbins <tyler@familyrobbins.com>
2015-06-21 14:37:35 -0700

    Added header file for DungeonVania.

---
+++ DungeonVania.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+#include <string.h>
+
+/*
+#include "SaveGameUtils.h"
+#include "Dungeon.h"
+#include "Shop.h"
+*/
+class Player;
+class Dungeon;
+
+#include "Player.h"
+
+static void createNewPlayer(Player* p);
+static bool yesNo();
+static void getMenuText();
+static void printPlayerInventoryWithFormatting(Player p);
+static void endGame();
+static void deathMessage(Dungeon d);
+static void goToDungeon();
+static void goToStore();
+static void getMenu(int choice, Player p);
