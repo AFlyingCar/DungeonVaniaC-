0d12f180f5426a9063b2885e38945e81b6cd142d
Tyler Robbins <tyler@familyrobbins.com>
2015-06-22 20:56:03 -0700

    Added Enemy class.

---
+++ Enemy.cpp
@@ -0,0 +1,83 @@
+#include "Enemy.h"
+#include "Player.h"
+
+int Enemy::m_count = 0;
+int Enemy::m_enemyInRoom = 0;
+
+//Enemy::Enemy(const Room& r):
+Enemy::Enemy():
+	m_health(0),
+	m_money(0),
+	m_name(""),
+	m_defense(0),
+	m_damage(0)/*,*/
+	//m_room(r)
+{
+	m_count++;
+	generateHealth();
+	generateMoney();
+	//generateDefense();
+	generateDamage();
+	generateName();
+}
+
+Enemy::~Enemy(){}
+
+void Enemy::addHealth(int newHealth){
+	m_health += newHealth;
+}
+void Enemy::setHealth(int newHealth){
+	m_health = newHealth;
+}
+
+int Enemy::getHealth(){ return m_health; }
+int Enemy::getDamage(){ return m_damage; }
+std::string Enemy::getName(){ return m_name; }
+int Enemy::getDefense(){ return m_defense; }
+int Enemy::getMoney(){ return m_money; }
+
+void Enemy::addDefense(int d){
+	m_defense += d;
+}
+
+void Enemy::defend(){
+	if(m_defense < 10)
+		m_defense++;
+}
+
+int Enemy::damagePlayer(Player *p){
+	int damageAmt = (m_damage - p->getArmourAbsorption());
+	p->addHealth(-damageAmt);
+	return damageAmt;
+}
+
+bool Enemy::isDead(){
+	return m_health <= 0;
+}
+
+void Enemy::generateHealth(){
+	srand(time(NULL));
+	m_health = (int)(rand() % (m_enemyInRoom+10)+10);
+}
+
+void Enemy::generateMoney(){
+	srand(time(NULL));
+	m_money = (int)(rand() % (m_enemyInRoom+2)+2);
+}
+
+// Commented out to prevent compiler stupidity
+/*
+void Enemy::generateDefense(){
+	srand(time(NULL));
+	m_defense = (int)(rand() % (2*m_count*(m_enemyInRoom/10.0f)));
+}
+*/
+void Enemy::generateDamage(){
+	srand(time(NULL));
+	m_damage = (int)(rand() % (10-m_enemyInRoom)+3);
+}
+
+// Generates a unique name for each enemy. Guaranteed to be random.
+void Enemy::generateName(){
+	m_name = "Enemy";
+}
---
+++ Enemy.h
@@ -0,0 +1,42 @@
+#ifndef _ENEMY_H_
+#define _ENEMY_H_
+
+#include <string>
+
+class Player;
+class Room;
+
+class Enemy{
+public:
+	static int m_enemyInRoom;
+	//Enemy(const Room& r);
+	Enemy();
+	~Enemy();
+	void addHealth(int newHealth);
+	void setHealth(int newHealth);
+	int getHealth();
+	int getDamage();
+	std::string getName();
+	int getDefense();
+	int getMoney();
+	void addDefense(int d);
+	void defend();
+	int damagePlayer(Player* p);
+	bool isDead();
+protected:
+	static int m_count;
+	int m_health;
+	int m_money;
+	std::string m_name;
+	int m_defense;
+	int m_damage;
+	//Room& m_room;
+
+	void generateHealth();
+	void generateMoney();
+	void generateDamage();
+	void generateDefense();
+	void generateName();
+};
+
+#endif
